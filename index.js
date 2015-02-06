919103f3efb0ab6d7c2f5a95eb52dd58fd25081c
Brian Di Palma <offler@gmail.com>
2015-02-06 08:16:32 +0000

    Strip other use strict pragmas in the module.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var rocamboleToken = require('rocambole-token');
+
 exports.stringAfter = function(formattedString) {
 	return "'use strict';\n\n" + formattedString;
 };
+
+exports.tokenBefore = function(token) {
+	if (isUseStrictPragma(token)) {
+		if (rocamboleToken.isSemiColon(token.next)) {
+			rocamboleToken.remove(token.next);
+		}
+
+		rocamboleToken.remove(token);
+	}
+};
+
+function isUseStrictPragma(token) {
+	return token.type === 'String' && (token.value === "'use strict'" || token.value === '"use strict"');
+}
