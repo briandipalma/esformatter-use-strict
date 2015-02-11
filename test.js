d99864a7110d507bf1d614a6a1ad6ba99996ca24
Brian Di Palma <offler@gmail.com>
2015-02-11 08:51:00 +0000

    Add tests to verify it removes other `use strict` pragmas.

--- test.js
+++ test.js
@@ -20,4 +20,26 @@ mocha.describe('use strict plugin', function() {
 		// Then.
 		assert.equal(formattedCode, "'use strict';\n\nfunction MyClass() {\n}");
 	});
+
+	mocha.it('should remove other use strict pragmas.', function() {
+		// Given.
+		var codeStr = "function MyClass() {'use strict';}";
+
+		// When.
+		var formattedCode = esformatter.format(codeStr);
+
+		// Then.
+		assert.equal(formattedCode, "'use strict';\n\nfunction MyClass() {\n}");
+	});
+
+	mocha.it('should remove other use strict pragmas in double quotes.', function() {
+		// Given.
+		var codeStr = 'function MyClass() {"use strict";\nvar t = 10;}';
+
+		// When.
+		var formattedCode = esformatter.format(codeStr);
+
+		// Then.
+		assert.equal(formattedCode, "'use strict';\n\nfunction MyClass() {\n  var t = 10;\n}");
+	});
 });
